5f0993659452672c6ce99ef67d566035f3fe2872
Aaron P. Olds <aaron.olds@gmail.com>
2018-09-06 15:55:33 -0400

    Added express and removed http

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-import { createServer } from 'http';
-const port = process.env.PORT || 3000;
+import express from "express";
 
-const server = createServer((req, res) => {
-  // res.statusCode = 200;
-  // res.setHeader('Content-Type', 'text/plain');
-  // res.end('Gday Git Deploy!\n');
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, world!\n'); 
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get("/", function(req, res) {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.send("hello world");
 });
 
-server.listen(port, () => {
+app.listen(port, function() {
   console.log(`Server running on http://localhost:${port}/`);
 });
+
+// below was using build in http above we are now using express
+
+// import { createServer } from 'http';
+// const port = process.env.PORT || 3000;
+
+// const server = createServer((req, res) => {
+//   // res.statusCode = 200;
+//   // res.setHeader('Content-Type', 'text/plain');
+//   // res.end('Gday Git Deploy!\n');
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello, world!\n');
+// });
+
+// server.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}/`);
+// });
